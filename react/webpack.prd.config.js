c44cd6a27dab92fe296c38b3d7bd61cc289dd88f
Calhoun5 <johnhckuo@gmail.com>
2018-04-22 16:39:24 +0800

    prod test

--- react/webpack.prd.config.js
+++ react/webpack.prd.config.js
@@ -11,7 +11,7 @@ module.exports = {
     index: './index.js'
   },
   output: {
-    path: path.resolve(__dirname, 'docs'),
+    path: path.resolve(__dirname, 'build'),
     filename: 'index.js'
   },
   module: {
